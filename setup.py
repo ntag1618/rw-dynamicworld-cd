c8a0d223db96aa2de00150bd436f89256c3fe516
Kristine Lister <31804766+kristinelister@users.noreply.github.com>
2020-12-16 14:16:39 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="rw-dynamicworld-cd", # Replace with your own username
+    name="rw-dynamicworld-cd", 
     version="0.0.1",
     author="Kristine Lister and Alex Kovac",
     author_email="kristine.lister@wri.org",
